07101fa1ffca00fa1730937060073b019f1c50af
BaleusBlueberry <ssaasshh29@gmail.com>
2024-08-18 18:17:33 +0300

    Add project files.

---
+++ Controllers/People.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using personListingsAPI.Servises;
+
+namespace personListingsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class People : ControllerBase
+    {
+        private readonly MongoService mongo;
+        private readonly IMongoCollection<People> people;
+
+        public People(MongoService mongo)
+        {
+            this.mongo = mongo;
+            people = mongo.GetCollection<People>("people");
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(people.Find(p => true).ToList());
+        }
+
+    }
+
+}
---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace personListingsAPI.Models
+{
+    public class Person
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
+
